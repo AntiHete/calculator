b0bc6dde7ee9ff07439e78b3483c9212a39e7b15
Glimsura <antihete12@gmail.com>
2024-09-29 13:00:08 +0300

    added more operations

--- calculator.h
+++ calculator.h
@@ -4,8 +4,11 @@
 class Calculator
 {
     public:
-        int Add (int, int);
-        int Mul (double, double);
+        double Add(double, double);
+        double Sub(double, double);
+        double Mul(double, double);
+        double Div(double, double);
+        double Pow(double, int);
 };
 
-#endif//CALCULATOR_H
+#endif // CALCULATOR_H
